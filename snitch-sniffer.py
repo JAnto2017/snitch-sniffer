fcb1b27bea2a1f5961b2fed2d96b7ff52ef45798
JAnto2017 <cursooperaciones2017@gmail.com>
2019-03-03 16:35:01 +0000

    add json rules and python program

---
+++ snitch-sniffer.py
@@ -0,0 +1,17 @@
+  
+
+Welcome to GNU Emacs, one component of the GNU/Linux operating system.
+
+Emacs Tutorial	Learn basic keystroke commands (Tutorial de Emacs)
+Emacs Guided Tour	Overview of Emacs features at gnu.org
+View Emacs Manual	View the Emacs manual using Info
+Absence of Warranty	GNU Emacs comes with ABSOLUTELY NO WARRANTY
+Copying Conditions	Conditions for redistributing and changing Emacs
+Ordering Manuals	Purchasing printed copies of manuals
+
+To start...     Open a File     Open Home Directory     Customize Startup
+To quit a partially entered command, type Control-g.
+
+This is GNU Emacs 24.5.1 (arm-unknown-linux-gnueabihf, GTK+ Version 3.22.11)
+ of 2017-09-13 on bm-wb-04, modified by Debian
+Copyright (C) 2015 Free Software Foundation, Inc.
